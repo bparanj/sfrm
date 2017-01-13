74a2a301a74a867456bcb746d023aad278f732fc
Bala Paranj <bparanj@gmail.com>
2017-01-13 10:51:15 -0800

    first commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+Task.create(name: 'Get rich quick')
+Task.create(name: 'Buy puppy')
+Task.create(name: 'Write a book')
+Task.create(name: 'Make money')
+Task.create(name: 'Become smart')
+Task.create(name: 'Buy weed')
+Task.create(name: 'Buy stingray blocker')
+Task.create(name: 'Pretend to be smart')
